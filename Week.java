5d25d843fe708764ae816c077b81f4a3fb71c6fa
kavinT1995 <kavin.tcse@ksrce.ac.in>
2016-08-11 13:40:49 +0530

    Create Week.java

---
+++ Week.java
@@ -0,0 +1,15 @@
+# wee
+public class Week {
+
+	
+	public static void main(String[] args) {
+		Scanner sr=new Scanner(System.in);
+		String s=sr.nextLine();
+		if(s=="sunday"){
+			System.out.println("false");
+		}
+		else
+			System.out.println("true");
+	}
+
+}
